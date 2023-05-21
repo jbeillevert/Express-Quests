136b0ee14653922304b906a5f9106f1a80af6195
J-E <beillevertj.contact@gmail.com>
2023-05-21 14:56:52 +0200

    quete 4 express terminée

--- userHandler.js
+++ userHandler.js
@@ -40,11 +40,34 @@ const postUser = (req, res) => {
     });
 }
 
+const updateUser = (req, res) => {
+    const id = parseInt(req.params.id)
+    const { firstname, lastname, email, city, language } = req.body;
+  
+    database
+      .query(
+        "UPDATE users SET firstname = ?, lastname = ?, email = ?, city = ?, language = ?",
+        [firstname, lastname, email, city, language, id]
+      )
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.status(404).send("Not Found")        
+        } else {
+          res.sendStatus(204)
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error editing the user");
+      });
+  };
+
 
 module.exports = {
     getUsers,
     getUserById,
-    postUser
+    postUser,
+    updateUser,
 }
 
     
